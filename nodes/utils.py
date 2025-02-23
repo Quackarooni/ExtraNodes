6bd414ac47f83a5411f4be0976af2656eff3d70a
Quackers <quackadoobers@gmail.com>
2025-02-23 21:54:05 +0800

    Utils: Add draw function for a standard image selector

--- nodes/utils.py
+++ nodes/utils.py
@@ -2,3 +2,7 @@ def debug_draw_group(self, layout):
     row = layout.row()
     row.enabled = False
     row.template_ID(self, "node_tree")
+
+
+def UI_image_selector(self, layout, prop_name):
+    layout.template_ID(self, prop_name, new="image.new", open="image.open")
